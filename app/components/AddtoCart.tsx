8db726b73142bf2b654a924240b698271247fc73
codegreene93 <34109141+codegreene93@users.noreply.github.com>
2024-05-02 20:41:38 +0100

    add data fetching to server side rendering

---
+++ app/components/AddtoCart.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react'
+
+const AddtoCart = () => {
+  return (
+    <div>
+    <button onClick={() => console.log("Add to cart")}> Add to cart</button>
+    </div>
+  )
+}
+
+export default AddtoCart
