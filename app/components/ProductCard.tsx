ea85b12cd59ae33a0e5a06f8c12bc87bba8c31e6
codegreene93 <34109141+codegreene93@users.noreply.github.com>
2024-05-03 20:20:32 +0100

    replace with tailwind

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import AddtoCart from './AddtoCart';
-import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600" >
         <AddtoCart />
     </div>
   )
